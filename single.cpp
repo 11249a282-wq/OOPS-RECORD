5972dcd71e5c0e8e2148ce117d4d422a3dc2ed66
11249a282-wq <11249a282@kanchiuniv.ac.in>
2025-11-09 23:50:42 -0800

    Add files via upload

---
+++ single.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+    cout<<"content.type.text/html\n\n";
+    cout<<"<html><body>";
+    cout<<"<h2>hello from c++ web cgt program</h2>";
+    cout<<"</body></html>";
+    return 0;
+}
